845c1b5cf44cf623eefcbfe21867ae1c4f462f2c
Gowri <Gowri@192.168.1.3>
2025-09-10 18:20:46 +0530

    maximized browser using window options

---
+++ Selenium/src/test/java/browseroptions/MaximizeBrowser.java
@@ -0,0 +1,17 @@
+package browseroptions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MaximizeBrowser {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.quit();
+
+	}
+
+}
